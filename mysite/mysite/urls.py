11ff6e405752c0f308099c2986a199ba9fe02043
Konstantina Souvatzidaki <kwn.souv@gmail.com>
2021-12-10 23:55:28 +0200

    add first path and view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('polls/', include('polls.urls')), #localhost:8000/polls ==> go to polls.urls ==> get "index" view from views.py
     path('admin/', admin.site.urls),
 ]
