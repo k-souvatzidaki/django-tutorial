ba3ce41a2d948c1ca8867ea2232ac0b7a8a14b56
Konstantina Souvatzidaki <kwn.souv@gmail.com>
2021-12-21 22:47:35 +0200

    generic urls and templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,34 +2,48 @@ from django.shortcuts import render,  get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
-# homepage
-def index(request):
-    question_list = Question.objects.order_by('-pub_date')[:5] # returns 5 most recent questions
-    # template = loader.get_template('polls/index.html') # load template
-    context = {
-        'latest_question_list': question_list, # pass argument to template : the questions list
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
-
-
-# a question page
-def detail(request, question_id):
-    #try:
-    #    question = Question.objects.get(pk=question_id)
-    #except Question.DoesNotExist:
-    #    raise Http404("Question does not exist")
-    
-    # BETTER ALTERNATIVE to maintain loose coupling : get_object_or_404
-    question = get_object_or_404(Question, pk=question_id) # calls get() function of the model’s manager, raises Http404 if the object doesn’t exist.
-    return render(request, 'polls/detail.html', {'question': question})
+# without generic views 
+# ---------------------
+#def index(request):
+#    question_list = Question.objects.order_by('-pub_date')[:5] # returns 5 most recent questions
+#    context = {
+#        'latest_question_list': question_list, # pass argument to template : the questions list
+#    }
+#    return render(request, 'polls/index.html', context)
 
 
-# results of question page
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+#def detail(request, question_id):
+#    # to maintain loose coupling : get_object_or_404 (instead of try except)
+#    question = get_object_or_404(Question, pk=question_id) # calls get() function of the model’s manager, raises Http404 if the object doesn’t exist.
+#    return render(request, 'polls/detail.html', {'question': question})
+
+
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question': question})
+
+
+# with generic views 
+# ---------------------
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 # vote action
